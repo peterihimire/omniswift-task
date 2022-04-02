d4171783e53e1fa0535e58c5fea6d0d0309c75d0
Peter Ihimire <peterihimire@gmail.com>
2022-04-02 15:18:33 +0100

    Fixed table row nth child not styling by not mapping result array direct instead I got out the elements one by one and gave them individual row styles

--- src/components/home/studentTable/index.js
+++ src/components/home/studentTable/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../../../store/actions";
 import Loader from "react-loader-spinner";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 import "./styles.scss";
 
 const StudentsTable = () => {
@@ -20,129 +20,6 @@ const StudentsTable = () => {
       loading: state.students.loading,
     };
   });
-
-  const data = [
-    {
-      id: "2",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2l",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2t",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "male",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2l",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2t",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "male",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2l",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2t",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "male",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2l",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "maleed",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-    {
-      id: "2t",
-      surname: "Ihimire",
-      firstName: "Peter",
-      age: "24",
-      gender: "male",
-      level: "100",
-      state: "Lagos",
-      action: "https://github.com/",
-    },
-  ];
 
   const studentsData = allStudents;
   console.log(studentsData);
@@ -168,58 +45,64 @@ const StudentsTable = () => {
     <section className='transactionHistory'>
       <div className='wrapper'>
         <div className='content'>
-          <table>
-            <tbody>
-              <tr>
-                <th>S/N</th>
-                <th>Surname</th>
-                <th>First Name</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Level</th>
-                <th>State</th>
-                <th>Action</th>
-              </tr>
-              {studentsData.map((val, key) => {
-                return (
-                  <tr key={key}>
-                    <td className='' width='5%'>
-                      {key + 1}
-                    </td>
-                    <td className='' width='12%'>
-                      {val.surname}
-                    </td>
-                    <td className='' width='12%'>
-                      {val.firstname}
-                    </td>
-                    <td className='' width='8%'>
-                      {val.age}
-                    </td>
-                    <td className='' width='8%'>
-                      {val.gender}
-                    </td>
-                    <td className='' width='8%'>
-                      {val.level}
-                    </td>
-                    <td className='' width='12%'>
-                      {val.state}
-                    </td>
-                    <td width='12%'>
-                      <button
-                        // to={`/result/${val.id}`}
-                        className='download-btn btn-primary'
-                        onClick={() => {
-                          history.push(`/result/${val.id}`);
-                        }}
-                      >
-                        Download Result
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          {studentsData.length === 0 ? (
+            <div className='no-result'>
+              <h3>NO RESULT AVAILABLE</h3>
+            </div>
+          ) : (
+            <table>
+              <tbody>
+                <tr>
+                  <th>S/N</th>
+                  <th>Surname</th>
+                  <th>First Name</th>
+                  <th>Age</th>
+                  <th>Gender</th>
+                  <th>Level</th>
+                  <th>State</th>
+                  <th>Action</th>
+                </tr>
+                {studentsData.map((val, key) => {
+                  return (
+                    <tr key={key}>
+                      <td className='' width='5%'>
+                        {key + 1}
+                      </td>
+                      <td className='' width='12%'>
+                        {val.surname}
+                      </td>
+                      <td className='' width='12%'>
+                        {val.firstname}
+                      </td>
+                      <td className='' width='8%'>
+                        {val.age}
+                      </td>
+                      <td className='' width='8%'>
+                        {val.gender}
+                      </td>
+                      <td className='' width='8%'>
+                        {val.level}
+                      </td>
+                      <td className='' width='12%'>
+                        {val.state}
+                      </td>
+                      <td width='12%'>
+                        <button
+                          // to={`/result/${val.id}`}
+                          className='download-btn btn-primary'
+                          onClick={() => {
+                            history.push(`/result/${val.id}`);
+                          }}
+                        >
+                          Download Result
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </section>
--- src/components/result/index.js
+++ src/components/result/index.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../../store/actions";
 import Loader from "react-loader-spinner";
+import ReactToPrint from "react-to-print";
 
 import "./styles.scss";
 
 const Result = (props) => {
+  const [studentRes, setStudentRes] = useState({});
+  const [studentRes2, setStudentRes2] = useState({});
+  const [studentRes3, setStudentRes3] = useState({});
+  const [studentRes4, setStudentRes4] = useState({});
+  // const [width] = useState(641);
   console.log(props);
   console.log(props.match.params.id);
   const location = useLocation();
   const dispatch = useDispatch();
+  const componentRef = useRef();
   console.log(location);
 
   const [id, setId] = useState("");
 
   const { result, loading, error } = useSelector((state) => {
     console.log(state);
-    // console.log(error, loading);
-    // console.log(allStudents);
+
     return {
       result: state.students.result,
       error: state.students.error,
@@ -35,7 +41,24 @@ const Result = (props) => {
     dispatch(actions.result({ id }));
   }, [dispatch, id]);
 
-  console.log(result);
+  // useEffect(() => {
+  //   if (window.innerWidth < width) {
+  //     alert("Place your phone in landscape mode for the best experience");
+  //   }
+  // }, [width]);
+  // console.log(result);
+  // console.log(width);
+
+  useEffect(() => {
+    let studentsResult = result.data?.result[0];
+    let studentsResult2 = result.data?.result[1];
+    let studentsResult3 = result.data?.result[2];
+    let studentsResult4 = result.data?.result[3];
+    setStudentRes(studentsResult);
+    setStudentRes2(studentsResult2);
+    setStudentRes3(studentsResult3);
+    setStudentRes4(studentsResult4);
+  }, [result.data?.result]);
 
   if (loading)
     return (
@@ -50,68 +73,215 @@ const Result = (props) => {
       </div>
     );
 
+  const resultWrapper = {
+    padding: "40px 20px",
+  };
+  const resultHeader = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
+  const logoDiv = {
+    width: "100px",
+    height: "100px",
+  };
+  const resultAddress = {
+    textAlign: "center",
+    margin: "0px 10px",
+  };
+  const schAddress = {};
+  const schDept = {};
+  const passportDiv = {
+    width: "100px",
+    height: "100px",
+  };
+  const resultUser = {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "50px 0",
+  };
+  const resultUserItem = {
+    display: "flex",
+    alignItem: "center",
+    marginBottom: "20px",
+  };
+  const table1 = {
+    width: "100%",
+    height: "330px",
+    marginBottom: "20px",
+    backgroundColor: "white",
+  };
+  const mTable1 = {
+    width: "100%",
+    height: "200px",
+    borderCollapse: "collapse",
+  };
+  const table2 = {
+    width: "80%",
+    height: "150px",
+  };
+  const mTable2 = {
+    width: "100%",
+    height: "100px",
+    borderCollapse: "collapse",
+  };
+  const remarks = {
+    marginBottom: "100px",
+  };
+  const remarksItem = {
+    display: "flex",
+    alignItem: "center",
+  };
+  const signatureWrapper = {};
+  const signature = {
+    width: "200px",
+    marginBottom: "20px",
+    border: "solid 1px black",
+  };
+
+  const tHead = {
+    backgroundColor: "#0D7590",
+    color: "white",
+    padding: "15px 10px",
+    marginBottom: "10px",
+  };
+
+  const tRow1 = {
+    backgroundColor: "#f2f2f2",
+  };
+  const tRow2 = {
+    backgroundColor: "#ffffff",
+  };
+  const tRow3 = {
+    backgroundColor: "#f2f2f2",
+  };
+  const tRow4 = {
+    backgroundColor: "#ffffff",
+  };
+  const tData = {
+    padding: "15px 10px",
+  };
+
+  const mainRemark = {
+    color: "#0D7590",
+    marginLeft: "5px",
+  };
+
+  const span = {
+    marginLeft: "20px",
+  };
+  const addTitle = {
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: "20px",
+    lineHeight: "19px",
+    color: "#4f4f4f",
+    marginBottom: "10px",
+  };
+
+  const addPara = {
+    width: "270px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "10px",
+  };
+
+  const deptTitle = {
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "20px",
+    lineHeight: "24px",
+    color: "#333333",
+    marginBottom: "10px",
+  };
+
+  const deptSubTitle = {
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: "12px",
+    lineHeight: "14px",
+    color: "#333333",
+    marginBottom: "10px",
+  };
+
   return (
     <div className='result'>
       <div className='wrapper'>
-        <div className='result-wrapper'>
-          <div className='result-header'>
-            <div className='logo-div'>
+        <div className='save-btn-div'>
+          <ReactToPrint
+            trigger={() => (
+              <button className='btn-primary save-btn'>SAVE FILE</button>
+            )}
+            content={() => componentRef.current}
+          />
+        </div>
+
+        <div
+          id='content'
+          className='result-wrapper'
+          ref={componentRef}
+          style={resultWrapper}
+        >
+          <div className='result-header' style={resultHeader}>
+            <div className='logo-div' style={logoDiv}>
               <img src={result.logo} alt='' />
             </div>
-            <div className='result-address'>
-              <div className='sch-address'>
-                <h2>FREMONT COLLEGE OF EDUCATION</h2>
-                <p>
+            <div className='result-address' style={resultAddress}>
+              <div className='sch-address' style={schAddress}>
+                <h2 style={addTitle}>FREMONT COLLEGE OF EDUCATION</h2>
+                <p style={addPara}>
                   No.5 Raymond Osuman Street, PMB 2191 Maitama, Abuja, Nigeria.
                 </p>
               </div>
-              <div className='sch-dept'>
-                <h2>Post Graduate Diploma in Education</h2>
-                <h5>Student First Semester Statement Of Result</h5>
+              <div className='sch-dept' style={schDept}>
+                <h2 style={deptTitle}>Post Graduate Diploma in Education</h2>
+                <h5 style={deptSubTitle}>
+                  Student First Semester Statement Of Result
+                </h5>
               </div>
             </div>
-            <div className='passport-div'>
+            <div className='passport-div' style={passportDiv}>
               <img src={result.profile_picture} alt='' />
             </div>
           </div>
 
-          <div className='result-user'>
+          <div className='result-user' style={resultUser}>
             <div>
-              <div className='result-user-item'>
+              <div className='result-user-item' style={resultUserItem}>
                 <h4>Name:</h4>
-                <span>{result.data?.surname}</span>
-                <span>{result.data?.firstname}</span>
+                <span style={span}>
+                  {result.data?.surname + " " + result.data?.firstname}
+                </span>
               </div>
-              <div className='result-user-item'>
+              <div className='result-user-item' style={resultUserItem}>
                 <h4>level:</h4>
-                <span>{result.data?.level}</span>
+                <span style={span}>{result.data?.level}</span>
               </div>
             </div>
 
             <div>
-              <div className='result-user-item'>
+              <div className='result-user-item' style={resultUserItem}>
                 <h4>Reg. No.:</h4>
-                <span>{result.data?.reg_no}</span>
+                <span style={span}>{result.data?.reg_no}</span>
               </div>
-              <div className='result-user-item'>
+              <div className='result-user-item' style={resultUserItem}>
                 <h4>Session:</h4>
-                <span>{result.data?.session}</span>
+                <span style={span}>{result.data?.session}</span>
               </div>
             </div>
           </div>
 
-          <div className='table1'>
-            <table>
+          <div className='table1' style={table1}>
+            <table style={mTable1}>
               <tbody>
                 <tr>
-                  <th>S/N</th>
-                  <th>Course Code</th>
-                  <th>Course Title</th>
-                  <th>Unit</th>
-                  <th>Grade</th>
-                  <th>Total Point</th>
+                  <th style={tHead}>S/N</th>
+                  <th style={tHead}>Course Code</th>
+                  <th style={tHead}>Course Title</th>
+                  <th style={tHead}>Unit</th>
+                  <th style={tHead}>Grade</th>
+                  <th style={tHead}>Total Point</th>
                 </tr>
-                {result.data?.result?.map((val, key) => {
+                {/* {result.data?.result?.map((val, key) => {
                   return (
                     <tr key={key}>
                       <td>{key + 1}</td>
@@ -124,44 +294,131 @@ const Result = (props) => {
                       <td>{val.total_point}</td>
                     </tr>
                   );
-                })}
+                })} */}
+
+                <tr style={tRow1}>
+                  <td style={tData}>{"1"}</td>
+                  <td style={tData}>{studentRes?.coursecode}</td>
+                  <td style={tData} className='' width='40%'>
+                    {studentRes?.title}
+                  </td>
+                  <td style={tData} className=''>
+                    {studentRes?.credit_unit}
+                  </td>
+                  <td style={tData}>{studentRes?.grade}</td>
+                  <td style={tData}>{studentRes?.total_point}</td>
+                </tr>
+
+                <tr style={tRow2}>
+                  <td style={tData}>{"2"}</td>
+                  <td style={tData}>{studentRes2?.coursecode}</td>
+                  <td style={tData} className='' width='40%'>
+                    {studentRes2?.title}
+                  </td>
+                  <td style={tData} className=''>
+                    {studentRes2?.credit_unit}
+                  </td>
+                  <td style={tData}>{studentRes2?.grade}</td>
+                  <td style={tData}>{studentRes2?.total_point}</td>
+                </tr>
+
+                <tr style={tRow3}>
+                  <td style={tData}>{"3"}</td>
+                  <td style={tData}>{studentRes3?.coursecode}</td>
+                  <td style={tData} className='' width='40%'>
+                    {studentRes3?.title}
+                  </td>
+                  <td style={tData} className=''>
+                    {studentRes3?.credit_unit}
+                  </td>
+                  <td style={tData}>{studentRes3?.grade}</td>
+                  <td style={tData}>{studentRes3?.total_point}</td>
+                </tr>
+
+                <tr style={tRow4}>
+                  <td style={tData}>{"4"}</td>
+                  <td style={tData}>{studentRes4?.coursecode}</td>
+                  <td style={tData} className='' width='40%'>
+                    {studentRes4?.title}
+                  </td>
+                  <td style={tData} className=''>
+                    {studentRes4?.credit_unit}
+                  </td>
+                  <td style={tData}>{studentRes4?.grade}</td>
+                  <td style={tData}>{studentRes4?.total_point}</td>
+                </tr>
+
+                {/* <tr>
+                  <td>{"2"}</td>
+                  <td>{studentRes[1]?.coursecode}</td>
+                  <td className='' width='40%'>
+                    {studentRes[1]?.title}
+                  </td>
+                  <td className=''>{studentRes[1]?.credit_unit}</td>
+                  <td>{studentRes[1]?.grade}</td>
+                  <td>{studentRes[1]?.total_point}</td>
+                </tr>
+                <tr>
+                  <td>{"3"}</td>
+                  <td>{studentRes[2]?.coursecode}</td>
+                  <td className='' width='40%'>
+                    {studentRes[2]?.title}
+                  </td>
+                  <td className=''>{studentRes[2]?.credit_unit}</td>
+                  <td>{studentRes[2]?.grade}</td>
+                  <td>{studentRes[2]?.total_point}</td>
+                </tr>
+                <tr>
+                  <td>{"4"}</td>
+                  <td>{studentRes[3]?.coursecode}</td>
+                  <td className='' width='40%'>
+                    {studentRes[3]?.title}
+                  </td>
+                  <td className=''>{studentRes[3]?.credit_unit}</td>
+                  <td>{studentRes[3]?.grade}</td>
+                  <td>{studentRes[3]?.total_point}</td>
+                </tr> */}
               </tbody>
             </table>
           </div>
 
-          <div className='table2'>
-            <table>
+          <div className='table2' style={table2}>
+            <table style={mTable2}>
               <tbody>
                 <tr>
-                  <th>UNTS</th>
-                  <th>UNTD</th>
-                  <th>GPTS</th>
-                  <th>GPTD</th>
-                  <th>GPATS</th>
-                  <th>GPATD</th>
+                  <th style={tHead}>UNTS</th>
+                  <th style={tHead}>UNTD</th>
+                  <th style={tHead}>GPTS</th>
+                  <th style={tHead}>GPTD</th>
+                  <th style={tHead}>GPATS</th>
+                  <th style={tHead}>GPATD</th>
                 </tr>
 
-                <tr>
-                  <td>{result.data?.cummulative?.unts}</td>
-                  <td>{result.data?.cummulative?.untd}</td>
-                  <td>{result.data?.cummulative?.gpts}</td>
-                  <td className=''>{result.data?.cummulative?.gptd}</td>
-                  <td>{result.data?.cummulative?.gpats}</td>
-                  <td>{result.data?.cummulative?.gpatd}</td>
+                <tr style={tRow1}>
+                  <td style={tData}>{result.data?.cummulative?.unts}</td>
+                  <td style={tData}>{result.data?.cummulative?.untd}</td>
+                  <td style={tData}>{result.data?.cummulative?.gpts}</td>
+                  <td style={tData} className=''>
+                    {result.data?.cummulative?.gptd}
+                  </td>
+                  <td style={tData}>{result.data?.cummulative?.gpats}</td>
+                  <td style={tData}>{result.data?.cummulative?.gpatd}</td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <div className='remarks'>
-            <div className='remarks-item'>
+          <div className='remarks' style={remarks}>
+            <div className='remarks-item' style={remarksItem}>
               <h4>Remarks:</h4>
-              <span>{result.data?.cummulative?.remarks}</span>
+              <span style={mainRemark}>
+                {result.data?.cummulative?.remarks}
+              </span>
             </div>
           </div>
 
-          <div className='signature-wrapper'>
-            <div className='signature'></div>
+          <div className='signature-wrapper' style={signatureWrapper}>
+            <div className='signature' style={signature}></div>
             <p>Registra</p>
           </div>
         </div>
